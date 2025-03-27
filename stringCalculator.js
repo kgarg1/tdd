eb9f94c794bb7bf46c53ae751c2273eb671d4e95
kuldeep <kuldeepgarg62@gmail.com>
2025-03-27 21:07:09 +0530

    if output is not string then throw error

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,4 +1,7 @@
 function add(numbers) {
+    if (typeof numbers !== 'string') 
+        throw new Error("Input must be a string");
+
     if (!numbers) return 0;
     return parseInt(numbers) || 0;
 }
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,11 @@ const add = require("./stringCalculator");
 test("returns 0 for an empty string", () => {
     expect(add("")).toBe(0);
 });
+
+test("throws an error when input is not a string", () => {
+    expect(() => add(5)).toThrow("Input must be a string");
+    expect(() => add(null)).toThrow("Input must be a string");
+    expect(() => add(undefined)).toThrow("Input must be a string");
+    expect(() => add([])).toThrow("Input must be a string");
+    expect(() => add({})).toThrow("Input must be a string");
+});
