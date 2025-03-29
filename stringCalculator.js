4983e9fd4322c91feeef34677b742ef12b8e2d23
kuldeep <kuldeepgarg62@gmail.com>
2025-03-29 12:16:15 +0530

    failed or passed test case with new line

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,5 +1,5 @@
-function isValidCommaSeparatedNumbers(str) {
-    return /^[0-9]+(,[0-9]+)*$/.test(str);
+function isValidCommaSeparatedOrNewLineNumbers(str) {
+    return typeof str === "string" && /^[0-9]+([,\n][0-9]+)*$/.test(str.trim());
 }
 
 function add(numbers) {
@@ -8,10 +8,10 @@ function add(numbers) {
 
     if (!numbers) return 0;
 
-    if (!isValidCommaSeparatedNumbers(numbers))
-        throw new Error("Input must be a comma seperated string");
+    if (!isValidCommaSeparatedOrNewLineNumbers(numbers))
+        throw new Error("Input must be a comma seperated or new line string");
 
-    const arrayNumbers = numbers.split(',');
+    const arrayNumbers = numbers.split(/[\n,]/);
     
     return arrayNumbers.reduce((sum, acc) => (parseInt(sum) + parseInt(acc)), 0);
 }
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -26,6 +26,14 @@ describe('Allow the add method to handle any amount of numbers', () => {
         
         expect(add(commaSeparated)).toBe(sum);
     });
+
+    test("return sum from n new line seperated values", () => {
+        const numbers = Array.from({ length: 50000 }, () => Math.floor(Math.random() * 100));
+        const commaSeparated = numbers.join('\n');
+        const sum = numbers.reduce((acc, num) => acc + num, 0);
+        
+        expect(add(commaSeparated)).toBe(sum);
+    });
 });
 
 describe('Failed Simple String calculator with a method', () => {
@@ -38,13 +46,19 @@ describe('Failed Simple String calculator with a method', () => {
     });
 });
 
-describe('Failed comma seperated value', () => {
-    test("throws an error when input must be a comma seperated string", () => {
-        expect(() => add("1,3,,5,")).toThrow("Input must be a comma seperated string");
-        expect(() => add("1,,3,,5")).toThrow("Input must be a comma seperated string");
-        expect(() => add("(1,,3,,5")).toThrow("Input must be a comma seperated string");
-        expect(() => add("1,3-5")).toThrow("Input must be a comma seperated string");
-        expect(() => add("1.35")).toThrow("Input must be a comma seperated string");
+describe('Failed comma or new line seperated value', () => {
+    test("throws an error when input must be a comma seperated or new line string", () => {
+        expect(() => add("1,3,,5,")).toThrow("Input must be a comma seperated or new line string");
+        expect(() => add("1,,3,,5")).toThrow("Input must be a comma seperated or new line string");
+        expect(() => add("(1,,3,,5")).toThrow("Input must be a comma seperated or new line string");
+        expect(() => add("1,3-5")).toThrow("Input must be a comma seperated or new line string");
+        expect(() => add("1.35")).toThrow("Input must be a comma seperated or new line string");
+
+        expect(() => add("1,3\n\n5,")).toThrow("Input must be a comma seperated or new line string");
+        expect(() => add("1,,3\n,5")).toThrow("Input must be a comma seperated or new line string");
+        expect(() => add("(1,,3,,5\n")).toThrow("Input must be a comma seperated or new line string");
+        expect(() => add("1\n3-5")).toThrow("Input must be a comma seperated or new line string");
+        expect(() => add("\n1.35")).toThrow("Input must be a comma seperated or new line string");
     });
 });
 
